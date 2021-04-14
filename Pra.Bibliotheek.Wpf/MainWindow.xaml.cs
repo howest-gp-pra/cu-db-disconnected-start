5987ac62b3aec86ddfb74d877a573acb24999cb9
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-04-14 04:30:17 +0200

    Add project files.

---
+++ Pra.Bibliotheek.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Pra.Bibliotheek.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+
+        private void CmbFilterAuthor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void CmbFilterPublisher_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearFilterAuthor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnClearFilterPublisher_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void LstBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LstAuthors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearAuthor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAddAuthor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnEditAuthor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnDeleteAuthor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LstPublishers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearUitgever_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAddPublisher_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnEditPublisher_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnDeletePulisher_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
